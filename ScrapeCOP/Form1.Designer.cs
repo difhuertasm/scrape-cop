e9bc50449ae92e06c97216d89358631e1bd5053e
difhuertasm <huertas.diego@yahoo.com>
2026-04-16 13:16:53 -0500

    Se agrega el proyecto.

---
+++ ScrapeCOP/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace WebScrapping1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.button1.Location = new System.Drawing.Point(173, 139);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Convertir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(86, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese los valores de COP en el formato x,xxx";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 76);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Pesos Colombianos (COP) $:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.MintCream;
+            this.textBox1.Location = new System.Drawing.Point(268, 73);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(161, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 236);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Euro (EUR) €:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 299);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Dolar (USD) $:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 418);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(147, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Real Brasilero (BRL) R:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(56, 356);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(154, 16);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Peso Mexicano (MXN) $:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(56, 479);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 16);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Yen Japonés (JPY)  ¥: ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.MintCream;
+            this.textBox2.Location = new System.Drawing.Point(268, 233);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(161, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.MintCream;
+            this.textBox3.Location = new System.Drawing.Point(268, 290);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(161, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.MintCream;
+            this.textBox4.Location = new System.Drawing.Point(268, 347);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(161, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.MintCream;
+            this.textBox5.Location = new System.Drawing.Point(268, 410);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(161, 22);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.Color.MintCream;
+            this.textBox6.Location = new System.Drawing.Point(268, 470);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(161, 22);
+            this.textBox6.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(495, 528);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.LightYellow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "ScrapeCop (Conversor de Divisas)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ ScrapeCOP/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+
+
+
+namespace WebScrapping1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string textCop = textBox1.Text;
+                double cop;
+
+                //Validad si una entrada se ingresó con punto decimal y se corrige por coma decimal para su conversión a double.
+                if (textCop.Contains("."))
+                {
+                    textCop = textCop.Replace(".", ",");
+                }
+
+                //Valida si una entrada de texto es en efecto un valor numérico de tipo double.
+                if (double.TryParse(textCop, out cop))
+                {
+                    //Valida si una entrada de texto es un valor mayor a cero.
+                    if (cop >= 0)
+                    {
+                        /*Realización de web scrapin mediante html agility pack*/
+
+                        string url = "https://www.riamoneytransfer.com/es-us/rates-conversion/?From=COP&To=EUR&Amount=1"; //Enlace objetivo
+                        var web = new HtmlWeb(); //Definición del método html agilty pack
+                        var doc = web.Load(url); //Carga el html
+                        var spanNode = doc.DocumentNode.SelectSingleNode("//span[@class='']"); //Etiqueta donde se encuentra el contenido a extraer.
+                        string valor = spanNode.InnerText.Replace(".", ","); // Obtiene el valor en string y reemplaza punto por coma decimal
+                        double eurIn = Convert.ToDouble(valor);//Conversión a valor decimal
+                        double euro = Math.Round(eurIn * cop, 4); //Cálculo en Euros a partir del peso colombiano ingresado.
+
+
+                        string url2 = "https://www.riamoneytransfer.com/es-us/rates-conversion/?From=COP&To=USD&Amount=1";
+                        var web2 = new HtmlWeb();
+                        var doc2 = web2.Load(url2);
+                        var spanNode2 = doc2.DocumentNode.SelectSingleNode("//span[@class='']");
+                        string valor2 = spanNode2.InnerText.Replace(".", ",");
+                        double usdIn = Convert.ToDouble(valor2);
+                        double usd = Math.Round(usdIn * cop, 4);
+
+                        string url3 = "https://www.riamoneytransfer.com/es-us/rates-conversion/?From=COP&To=MXN&Amount=1";
+                        var web3 = new HtmlWeb();
+                        var doc3 = web3.Load(url3);
+                        var spanNode3 = doc3.DocumentNode.SelectSingleNode("//span[@class='']");
+                        string valor3 = spanNode3.InnerText.Replace(".", ",");
+                        double mxnIn = Convert.ToDouble(valor3);
+                        double mxn = Math.Round(mxnIn * cop, 4);
+
+                        string url4 = "https://www.riamoneytransfer.com/es-us/rates-conversion/?From=COP&To=BRL&Amount=1";
+                        var web4 = new HtmlWeb();
+                        var doc4 = web4.Load(url4);
+                        var spanNode4 = doc4.DocumentNode.SelectSingleNode("//span[@class='']");
+                        string valor4 = spanNode4.InnerText.Replace(".", ",");
+                        double brlIn = Convert.ToDouble(valor4);
+                        double brl = Math.Round(brlIn * cop, 4);
+
+                        string url5 = "https://www.riamoneytransfer.com/es-us/rates-conversion/?From=COP&To=JPY&Amount=1";
+                        var web5 = new HtmlWeb();
+                        var doc5 = web5.Load(url5);
+                        var spanNode5 = doc5.DocumentNode.SelectSingleNode("//span[@class='']");
+                        string valor5 = spanNode5.InnerText.Replace(".", ",");
+                        double jpyIn = Convert.ToDouble(valor5);
+                        double jpy = Math.Round(jpyIn * cop, 4);
+
+                        /* Asignación de valores a las correspondientes cajas de texto (salida).*/
+                        textBox2.Text = euro.ToString();
+                        textBox3.Text = usd.ToString();
+                        textBox4.Text = mxn.ToString();
+                        textBox5.Text = brl.ToString();
+                        textBox6.Text = jpy.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Debe ingresar un valor mayor a cero.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Debe ingresar un valor numérico y sin espacios.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show($"Ocurrió un error de tipo: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
